8ce973f9c194baab15fd2478ce4aa68bfbda7bbd
Dr.jacky <dr.jacky.2005@gmail.com>
2020-01-26 12:58:05 +0100

    Initial commit

---
+++ app/src/main/java/app/web/drjackycv/flickrmultiplatform/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.web.drjackycv.flickrmultiplatform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
