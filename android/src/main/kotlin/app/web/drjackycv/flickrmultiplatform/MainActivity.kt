4572938a15ff45c522a85c7eb2fac8c70f3322cd
Dr.jacky <dr.jacky.2005@gmail.com>
2020-02-05 18:23:16 +0100

    Use shared method on android app

--- android/src/main/kotlin/app/web/drjackycv/flickrmultiplatform/MainActivity.kt
+++ android/src/main/kotlin/app/web/drjackycv/flickrmultiplatform/MainActivity.kt
@@ -2,11 +2,19 @@ package app.web.drjackycv.flickrmultiplatform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.web.drjackycv.flickrmultiplatform.shared.HelloWorldMessage
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupUI()
     }
+
+    private fun setupUI() {
+        mainMessageTxt.text = HelloWorldMessage()
+    }
+
 }
