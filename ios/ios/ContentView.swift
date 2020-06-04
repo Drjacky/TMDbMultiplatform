17374582f21aff942a3628d638ff8094d9edd3be
Dr.jacky <dr.jacky.2005@gmail.com>
2020-06-04 22:03:56 +0200

    Generated files after running packForXcode task

---
+++ ios/ios/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ios
+//
+//  Created by Hossein Abbasi on 2/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
