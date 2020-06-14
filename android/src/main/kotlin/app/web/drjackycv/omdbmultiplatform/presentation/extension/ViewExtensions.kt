c0145a0c59c03683b85f20de023a3659c9142107
Dr.jacky <dr.jacky.2005@gmail.com>
2020-06-14 20:14:44 +0200

    Create view extension

---
+++ android/src/main/kotlin/app/web/drjackycv/omdbmultiplatform/presentation/extension/ViewExtensions.kt
@@ -0,0 +1,44 @@
+package app.web.drjackycv.omdbmultiplatform.presentation.extension
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import androidx.annotation.LayoutRes
+import androidx.appcompat.content.res.AppCompatResources
+import app.web.drjackycv.omdbmultiplatform.R
+import app.web.drjackycv.omdbmultiplatform.presentation.base.util.GlideApp
+import com.bumptech.glide.request.RequestOptions
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View =
+    LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+
+fun ImageView.load(
+    url: String,
+    @DrawableRes placeholderRes: Int = R.drawable.ic_cloud_download
+) {
+    val safePlaceholderDrawable = AppCompatResources.getDrawable(context, placeholderRes)
+    val requestOptions = RequestOptions().apply {
+        placeholder(safePlaceholderDrawable)
+        error(safePlaceholderDrawable)
+    }
+    val glideRequest = GlideApp
+        .with(context)
+        .setDefaultRequestOptions(requestOptions)
+        .load(url)
+
+    glideRequest.into(this)
+}
