4ec3ceaf21b2554dead1fcb958edd67d6ed3fde5
Dr.jacky <dr.jacky.2005@gmail.com>
2020-07-26 22:14:41 +0200

    Create manual coroutines interop

---
+++ shared/src/commonMain/kotlin/data/base/coroutines/JobAsDisposable.kt
@@ -0,0 +1,15 @@
+package data.base.coroutines
+
+import com.badoo.reaktive.disposable.Disposable
+import kotlinx.coroutines.Job
+
+fun Job.asDisposable(): Disposable =
+    object : Disposable {
+
+        override val isDisposed: Boolean get() = !isActive
+
+        override fun dispose() {
+            cancel()
+        }
+
+    }
