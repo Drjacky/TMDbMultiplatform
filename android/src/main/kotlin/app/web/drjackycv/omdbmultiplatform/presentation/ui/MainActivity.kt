527cf7d5c9b137473b47aa5cfae8ace87686cdac
Dr.jacky <dr.jacky.2005@gmail.com>
2020-07-26 22:51:17 +0200

    [WIP] Implement android part

--- android/src/main/kotlin/app/web/drjackycv/omdbmultiplatform/presentation/ui/MainActivity.kt
+++ android/src/main/kotlin/app/web/drjackycv/omdbmultiplatform/presentation/ui/MainActivity.kt
@@ -7,15 +7,34 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import app.web.drjackycv.omdbmultiplatform.BuildConfig
 import app.web.drjackycv.omdbmultiplatform.R
 import app.web.drjackycv.omdbmultiplatform.presentation.adapter.MoviesAdapter
+import app.web.drjackycv.omdbmultiplatform.presentation.entity.MovieUI
+import app.web.drjackycv.omdbmultiplatform.presentation.mapper.MoviesUIMapper
+import data.mapper.MoviesMapper
+import data.remote.MoviesApiImpl
+import data.repository.MoviesRepositoryImpl
+import domain.usecase.GetMoviesUseCaseImpl
+import presentation.base.ListViewModel
+import presentation.base.ViewModelBinding
+import presentation.movies.MoviesListViewModel
 
-
-private val API_KEY = BuildConfig.API_KEY
+private const val API_KEY = BuildConfig.API_KEY
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mMoviesAdapter: MoviesAdapter
     private lateinit var mRecyclerView: RecyclerView
     private lateinit var mRefreshLayout: SwipeRefreshLayout
+    private var mIsRefreshing = false
+    private val mBinding = ViewModelBinding()
+    private val mViewModel: ListViewModel<String, MovieUI> by lazy {
+        val moviesMapper = MoviesMapper()
+        val api = MoviesApiImpl("b445ca0b", "https://www.omdbapi.com/", moviesMapper)
+        val repository = MoviesRepositoryImpl(api)
+        val useCase = GetMoviesUseCaseImpl(repository)
+        val moviesUiMapper = MoviesUIMapper()
+
+        MoviesListViewModel(useCase, moviesUiMapper)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
