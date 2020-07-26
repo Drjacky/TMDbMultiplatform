8c14799d4f2682f1c572f178cb1bdec80da21e41
Dr.jacky <dr.jacky.2005@gmail.com>
2020-07-26 23:04:15 +0200

    [WIP] Create extension for viewmodel for later uses

---
+++ android/src/main/kotlin/app/web/drjackycv/omdbmultiplatform/presentation/extension/Extensions.kt
@@ -0,0 +1,18 @@
+package app.web.drjackycv.omdbmultiplatform.presentation.extension
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.*
+
+inline fun <reified T : ViewModel> Fragment.viewModel(
+    factory: ViewModelProvider.Factory,
+    body: T.() -> Unit = {}
+): T {
+    val vm = ViewModelProvider(this, factory).get(T::class.java)
+    vm.body()
+
+    return vm
+}
+
+fun <T> LifecycleOwner.observe(liveData: LiveData<T>, action: (t: T) -> Unit) {
+    liveData.observe(this, Observer { it?.let { t -> action(t) } })
+}
